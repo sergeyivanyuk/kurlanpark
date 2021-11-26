5af57c4bc7475fedef83658845b97a93058e7c6f
Sergey Ivanyuk <68748390+sergeyivanyuk@users.noreply.github.com>
2021-11-26 13:29:19 +0300

    add section

--- app/mail.php
+++ app/mail.php
@@ -3,14 +3,14 @@ if(isset($_POST['name']) && isset($_POST['phone']) && isset($_POST['email']) ) {
 	$name = $_POST['name'];
 	$tel = $_POST['phone'];
 	$email = $_POST['email'];
-	if(mail('sergeyivanyuk89@gmail.com',
+	if(mail('info@kurlanpark.ru',
 			'Заявка на звонок',
 			'Имя:'.$name."\n".
 			'Телефон:'.$tel."\n".
 			'E-mail:'.$email
 			)
 		) {
-		echo('Успешно отправлено, в ближайшее время мы вам перезвоним');
+		echo('Успешно отправлено');
 		header("Location: https://kurlanpark.ru");
 	}	else {
 		echo('Что-то пошло не так');
